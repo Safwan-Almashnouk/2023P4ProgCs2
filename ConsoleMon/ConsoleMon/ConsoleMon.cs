6266c8bdd8272112460df2886310e8707a8d4870
Safwan <35345@ma-web.nl>
2023-05-08 13:08:06 +0200

    les1

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class ConsoleMon
+    {
+        
+        
+            public int health;
+            public int energy;
+            public string name;
+
+            internal void TakeDamage(int damage)
+            {
+                health -= damage;
+
+            }
+            internal void DepleteEnergy(int energy)
+            {
+                this.energy -= energy;
+            }
+        }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Skill
+    {
+
+        public int damage;
+        public int energyCost;
+        public string name;
+
+
+        internal void UseOn(ConsoleMon tarrget, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            tarrget.TakeDamage(damage);
+        }
+    }
+    }
+
